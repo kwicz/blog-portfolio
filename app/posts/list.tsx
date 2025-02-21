5c4b2d8024b24b67ff2a3af642c5f35687380497
Katy Solovewicz <katy@solovewicz.com>
2025-02-21 14:12:17 -0800

    Update post and project navbar and layout

--- app/posts/list.tsx
+++ app/posts/list.tsx
@@ -4,11 +4,10 @@ import { useState } from 'react';
 import { Card } from '../components/card';
 import { Article } from './article';
 
-<<<<<<< HEAD:app/posts/list.tsx
-interface Post {
-  slug: string;
-  title: string;
-  published: boolean;
+import { Post as GeneratedPost } from '../../.contentlayer/generated/types';
+
+interface Post extends GeneratedPost {
+  views?: number;
 }
 
 interface PostListProps {
@@ -20,39 +19,23 @@ export default function PostList({ posts, views }: PostListProps) {
   const [searchTerm, setSearchTerm] = useState('');
 
   const filteredPosts = posts
-=======
-export default function BlogList({ blogs, views }) {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const filteredBlogs = blogs
->>>>>>> 99db3f2cfea3422d9ebdd4e8da483772db2ad914:app/blog/list.tsx
     .filter((p) => p.published)
     .filter((p) => p.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
-    <div className='mt-4'>
+    <div>
       <input
         type='text'
-<<<<<<< HEAD:app/posts/list.tsx
         placeholder='Search post titles...'
-=======
-        placeholder='Search blogs titles...'
->>>>>>> 99db3f2cfea3422d9ebdd4e8da483772db2ad914:app/blog/list.tsx
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         className='mt-4 p-2 w-full rounded bg-zinc-800 text-zinc-100'
       />
 
       <div className='grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3 mt-8'>
-<<<<<<< HEAD:app/posts/list.tsx
         {filteredPosts.map((post: Post) => (
           <Card key={post.slug}>
             <Article post={post} views={views[post.slug] ?? 0} />
-=======
-        {filteredBlogs.map((blog) => (
-          <Card key={blog.slug}>
-            <Article blog={blog} views={views[blog.slug] ?? 0} />
->>>>>>> 99db3f2cfea3422d9ebdd4e8da483772db2ad914:app/blog/list.tsx
           </Card>
         ))}
       </div>
